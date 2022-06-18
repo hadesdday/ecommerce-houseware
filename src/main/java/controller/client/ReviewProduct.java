16cbec7e26c3065d6602331a343e744f1b6427a1
Hieu Nguyen <nhocproaoe@gmail.com>
2022-06-18 11:58:38 +0700

    upload missing files

---
+++ src/main/java/controller/client/ReviewProduct.java
@@ -0,0 +1,41 @@
+package controller.client;
+
+import beans.Review;
+import properties.AssetsProperties;
+import services.ReviewServices;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "ReviewProduct", value = "/post/comment")
+public class ReviewProduct extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String pid = request.getParameter("pid");
+        String username = request.getParameter("username");
+        int rating = Integer.parseInt(request.getParameter("rating"));
+        String content = request.getParameter("content");
+        String currentUrl = request.getParameter("currentUrl");
+
+        boolean isErr = false;
+        Review r = new Review(pid, username, rating, content);
+
+        if (pid.isEmpty() || username.isEmpty() || rating < 0) isErr = true;
+
+        if (isErr) {
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+        } else {
+            if (ReviewServices.getInstance().addReview(r)) {
+                response.sendRedirect(currentUrl);
+            } else {
+                response.sendError(HttpServletResponse.SC_NOT_ACCEPTABLE);
+            }
+        }
+    }
+}
